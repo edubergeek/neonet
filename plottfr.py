ff7a876c7831a6c5270dddeca2d2e7826713cdd1
edubergeek <dodds@hawaii.edu>
2019-01-14 22:48:06 -1000

    Cleaned up and adjusted to fit with changes to mops2tfr

--- plottfr.py
+++ plottfr.py
@@ -6,21 +6,21 @@ import matplotlib.pyplot as plt
 XDim=32
 YDim=32
 ZDim=1
-PDim=65
+PDim=3
 
 pathTrain = '../data/train.tfr'  # The TFRecord file containing the training set
 pathValid = '../data/val.tfr'    # The TFRecord file containing the validation set
 pathTest = '../data/test.tfr'    # The TFRecord file containing the test set
 
-batchSize=8
-batchN=1
+batchSize=10
+batchN=10000
 
 with tf.Session() as sess:
     feature = {
-        'cutout': tf.FixedLenSequenceFeature(shape=[], dtype=tf.float32, allow_missing=True),
-        'isNEO': tf.FixedLenSequenceFeature(shape=[1], dtype=tf.float32, allow_missing=True),
-        'detID': tf.FixedLenSequenceFeature(shape=[], dtype=tf.string, allow_missing=True),
-        'params': tf.FixedLenSequenceFeature(shape=[], dtype=tf.float32, allow_missing=True)
+        'detID': tf.FixedLenFeature(shape=[], dtype=tf.string),
+        'isNEO': tf.FixedLenFeature(shape=[], dtype=tf.float32),
+        'cutout': tf.FixedLenFeature(shape=[YDim*XDim], dtype=tf.float32),
+        'params': tf.FixedLenFeature(shape=[PDim], dtype=tf.float32),
       }
         
     # Create a list of filenames and pass it to a queue
@@ -33,26 +33,22 @@ with tf.Session() as sess:
 
     """Parse a single record into x and y images"""
     x = features['cutout']
-    # unroll into a 2D array
-    x = tf.reshape(x, (YDim, XDim))
-    # use slice to crop the data to the model dimensions - powers of 2 are a factor
-    #x = tf.slice(x, (0, 0, 0, 0), (WStokes, YDim, XDim, ZStokes))
-    
     y = features['isNEO']
-    # use slice to crop the data
-    #y = tf.slice(y, (0), (1,1))
-    y = tf.cast(y, tf.float32)
-    y = tf.reshape(y, (1,1))
+    did = features['detID']
+    p = features['params']
+    #p = np.random.rand(65)
     # Any preprocessing here ...
 
-    #p = features['params']
-    #p = tf.reshape(p, (1, PDim))
-    #did = features['detID']
-    #did = tf.cast(y, tf.string)
     
     # Creates batches by randomly shuffling tensors
-    #X, Y, P = tf.train.batch([x, y, p], batch_size=batchSize)
-    X, Y = tf.train.batch([x, y], batch_size=batchSize)
+    #X, Y, P, ID = tf.train.batch([x, y, p, did], batch_size=batchSize)
+    ID, Y, X, P = tf.train.batch([did, y, x, p], batch_size=batchSize)
+
+    # unroll into a 2D array
+    #X = X[0]
+    #Y = Y[0]
+    #ID = ID[0]
+    #P = tf.reshape(P, (-1, PDim))
 
     # Initialize all global and local variables
     init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
@@ -61,18 +57,18 @@ with tf.Session() as sess:
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
 
-    plt.gray()
+    #plt.gray()
     # Now we read batches of images and labels and plot them
     for batch_index in range(batchN):
-        #image, actual, params = sess.run([X, Y, P])
-        image, actual = sess.run([X, Y])
-        print(image.shape, actual.shape)
-        for i in range(X.shape[0]):
-            #print(params[i][0])
-            print(actual[i][0])
-            img = image[i, 0:YDim,0:XDim]
-            plt.imshow(img)
-            plt.show()
+        detID, actual, image, params = sess.run([ID, Y, X, P])
+        for m in range(batchSize):
+            #print(detID[m])
+            print(actual[m])
+            #print(params[m])
+            #print(image.shape)
+            #img = np.reshape(image[m], (YDim, XDim))
+            #plt.imshow(img)
+            #plt.show()
 
 
     # Stop the threads
